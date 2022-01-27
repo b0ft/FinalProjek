219f62c44137f4630dea78d0e90e2f3de6470215
Aldy Syachranie <aldysyach17@gmail.com>
2022-01-27 22:59:59 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./Home";
+import LoginScreen from "./Login";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            {/* <StatusBar style="auto" translucent={false} /> */}
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ Home.jsx
@@ -0,0 +1,15 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View>
+            {/* <Button
+                onPress={() => navigation.navigate("Login")}
+                title="Login"
+            /> */}
+        </View>
+    );
+};
+
+export default HomeScreen;
